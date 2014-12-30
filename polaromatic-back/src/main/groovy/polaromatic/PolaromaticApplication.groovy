94f98907256121dcdf96a3aa5c8c17355f933b0d
Iván López <lopez.ivan@gmail.com>
2014-12-30 12:45:48 +0100

    Spring Boot application skeleton

---
+++ polaromatic-back/src/main/groovy/polaromatic/PolaromaticApplication.groovy
@@ -0,0 +1,13 @@
+package polaromatic
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class PolaromaticApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run PolaromaticApplication, args
+    }
+
+}
