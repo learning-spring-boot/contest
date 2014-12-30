38bace24a83fac72c805ba20590fc3a8e152eae5
Iván López <lopez.ivan@gmail.com>
2014-12-30 13:19:17 +0100

    Add photo conversion Spring Integration flow

--- polaromatic-back/src/main/groovy/polaromatic/PolaromaticApplication.groovy
+++ polaromatic-back/src/main/groovy/polaromatic/PolaromaticApplication.groovy
@@ -2,8 +2,10 @@ package polaromatic
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.ImportResource
 
 @SpringBootApplication
+@ImportResource("classpath:resources.xml")
 class PolaromaticApplication {
 
     static void main(String[] args) {
