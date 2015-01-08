cf171f51376628bc8ddef8d563c141d7b5589a99
Iván López <lopez.ivan@gmail.com>
2015-01-08 23:28:19 +0100

    Implement retrofit error handler with a closure

--- polaromatic-grooid/src/main/groovy/polaromatic/app/service/RestServiceFactory.groovy
+++ polaromatic-grooid/src/main/groovy/polaromatic/app/service/RestServiceFactory.groovy
@@ -1,15 +1,23 @@
 package polaromatic.app.service
 
+import de.greenrobot.event.EventBus
 import polaromatic.app.activity.SettingsActivity
+import polaromatic.app.service.events.BackendEvent
+import retrofit.ErrorHandler
 import retrofit.RestAdapter
+import retrofit.RetrofitError
 
 class RestServiceFactory {
 
     static <T> T getService(String baseUrl, Class<T> clazz) {
+        ErrorHandler eh = { RetrofitError cause ->
+            EventBus.default.post(new BackendEvent())
+            cause.response
+        }
 
-        new RestAdapter.Builder()
+        return new RestAdapter.Builder()
             .setEndpoint(baseUrl ?: SettingsActivity.DEFAULT_BACKEND_URL)
-            .setErrorHandler(new PolaromaticErrorHandler())
+            .setErrorHandler(eh)
             .build()
             .create(clazz)
     }
