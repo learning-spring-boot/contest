9a61396f92b0d309cd2b101341743f59c2aa3da1
S2-Team <git@s2team.codes>
2015-01-09 00:28:43 +0100

    Added Fongo to made the integration test run without mongodb

--- src/main/java/de/votesapp/VotesAppApplication.java
+++ src/main/java/de/votesapp/VotesAppApplication.java
@@ -5,12 +5,16 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import reactor.core.Environment;
 import reactor.core.Reactor;
 import reactor.core.spec.Reactors;
 import reactor.spring.context.config.EnableReactor;
+
+import com.github.fakemongo.Fongo;
+import com.mongodb.Mongo;
 
 @Configuration
 @ComponentScan
@@ -25,5 +29,27 @@ public class VotesAppApplication {
 	@Bean
 	public Reactor rootReactor(final Environment env) {
 		return Reactors.reactor().env(env).get();
+	}
+
+	// TODO: That should be use in the test (profile) only. Since we have not
+	// real database config, we let it here.
+	@Configuration
+	static class MongoDbTestConfiguration extends AbstractMongoConfiguration {
+
+		@Override
+		protected String getDatabaseName() {
+			return "test";
+		}
+
+		@Override
+		public Mongo mongo() {
+			return new Fongo("mongo").getMongo();
+		}
+
+		@Override
+		protected String getMappingBasePackage() {
+			return "de.votesapp";
+		}
+
 	}
 }
