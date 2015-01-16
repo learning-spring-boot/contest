de7f47520cae476bb13259165e451d79f1195a66
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2015-01-17 00:55:16 +0100

    Added metrics

--- src/main/java/com/maciejwalkowiak/mercury/core/message/MessageRepository.java
+++ src/main/java/com/maciejwalkowiak/mercury/core/message/MessageRepository.java
@@ -1,9 +1,12 @@
 package com.maciejwalkowiak.mercury.core.message;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 /**
  * MongoDB based repository for {@link Message}
  */
 interface MessageRepository extends MongoRepository<Message, String> {
+	@Query(count = true, value = "{ 'status' : ?0 }")
+	long countByStatus(Message.Status status);
 }
